a43e4da685103bd72c1cb4d5e04a75cdf3a40985
Mena WANG <ningwang25@gmail.com>
2022-10-09 18:52:40 +1100

    add Bar_pct_label

---
+++ Bar_pct_label.py
@@ -0,0 +1,19 @@
+from plotnine import *
+from plotnine.data import *
+import pandas as pd
+
+mtcars.head(5)
+
+(
+    ggplot(mtcars, aes(x='factor(cyl)', fill = 'factor(cyl)'))
+    + geom_bar()
+    + geom_text(
+        aes(label = after_stat('prop*100'),group=1),
+        stat = 'count',
+        nudge_y = 0.125,
+        va = 'bottom',
+        format_string = '{:.1f}%'
+        )
+    + theme_minimal()
+)
+
